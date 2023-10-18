5c9f3cfd3cf84f223791fc8f807de0d47b941212
r3vvy <145227974+r3vvy@users.noreply.github.com>
2023-10-18 23:58:51 +0300

    First commit
    add code for lab 2

---
+++ Lab2/Lab2CPP.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+
+    int i = 69;  
+
+    int number = 72; 
+
+    i = i + number; 
+
+    int array[3] = { 0,0,0 }; 
+
+    int* iPointer = &i; 
+
+    number = *iPointer; 
+
+    *iPointer = 15;
+
+    iPointer = &array[0]; 
+
+    iPointer += 2;
+
+    array[0] = 5;
+
+    *iPointer = 6;
+}
